d1524b9b7cd27e287f15effdcad6f86ece90148e
Sushmitacode12 <sushmitatripathi786@gmail.com>
2023-02-02 06:24:51 +0530

    Cart Added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from "./Components/Navbar/Navbar";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+
+function App() {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
